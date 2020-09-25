dd61ae2ceb50221dad2113621b25a80542ccfb0f
NeginAryapour <negin_aryapour@hotmail.com>
2020-09-25 14:25:00 +0330

    side bar drivers

--- src/views/Drivers.vue
+++ src/views/Drivers.vue
@@ -3,12 +3,15 @@
     <!-- <h1>Vertical Menu</h1> -->
 
     <div class="vertical-menu">
-      <a href="#" >Dashboard</a>
-      <a href="#" class="active">Drivers</a>
-      <a href="#">Cars</a>
-      <a href="#">setting </a>
-      <a href="#">Log out</a>
+      <div>hi</div> 
+      <a href="#dashboard" >Dashboard</a>
+      <a href="#drivers" class="active">Drivers</a>
+      <a href="#cars">Cars</a>
+      <a href="#setting">setting </a>
+      <a href="#logout">Log out</a>
     </div>
+
+    
   </div>
 </template>
 
@@ -18,8 +21,15 @@ export default {};
 
 <style>
 .vertical-menu {
-  width: 250px;
-  /* height: 1000px; */
+  height: 100%; /* Full-height: remove this if you want "auto" height */
+  width: 250px; /* Set the width of the sidebar */
+  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+  background-color: white; /* Black */
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 20px;
 }
 
 .vertical-menu a {
@@ -27,11 +37,18 @@ export default {};
   color: #d3d3d4;
   display: block;
   padding: 20px;
+  /* margin-top: 0px; */
   padding-left: 70px;
   text-decoration: none;
+  display: block;
   /* margin: inherit; */
 
 }
+
+/* @media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+} */
 
 .vertical-menu a:hover {
   /* background-color: ; */
